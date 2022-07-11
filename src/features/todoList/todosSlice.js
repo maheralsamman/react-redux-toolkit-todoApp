61dcef3a6e04d89b7fb1fd6907bc222adbd3705d
Maher Alsamman <56821558+maheralsamman@users.noreply.github.com>
2022-07-11 22:05:46 +0200

    Update todosSlice.js

--- src/features/todoList/todosSlice.js
+++ src/features/todoList/todosSlice.js
@@ -9,6 +9,7 @@ const todosSlice = createSlice({
   name: "todolist",
   initialState,
   reducers: {
+// add a todo to the state and to localstorage
     addTodo: (state, { payload }) => {
       state.push(payload);
       localStorageSet(state);
@@ -25,6 +26,7 @@ const todosSlice = createSlice({
         return updatedArr;
       });
     },
+// remove todo from the state and localstorage
     removeTodo: (state, { payload }) => {
       const updatedArr = state.filter((todo) => todo.id !== payload);
       localStorageSet(updatedArr);
