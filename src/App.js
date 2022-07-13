8e141edee17edd060281cceeffd8f3f987615d23
Maher Alsamman <56821558+maheralsamman@users.noreply.github.com>
2022-07-13 21:27:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Form from "./components/Form";
 import Todos from "./components/Todos";
 
-// all code removed after adding redux toolkit
+
 function App() {
   return (
     <div className="app">
